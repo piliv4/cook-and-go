86c96b31ec992ea5ed99fd62e91ab50b08e7e1e2
piliv4 <79758821+piliv4@users.noreply.github.com>
2023-02-20 19:00:01 +0100

    cambios en el header

--- src/components/layout/AdminHeader.tsx
+++ src/components/layout/AdminHeader.tsx
@@ -1,28 +1,50 @@
 import Link from "next/link";
+import { useState } from "react";
 
 const AdminHeader = () => {
+  const [selectedOption, setSelectedOption] = useState(-1);
   return (
     <div className="flex justify-center">
       <div className="bg-blue-600 py-2 px-8 flex flex-row mb-4 gap-x-3 rounded-b-full w-[95%] ">
         <h1 className="font-bold text-lg px-2 text-white">
-          <Link href={"/"}>Cook&Go</Link>
+          <Link href={"/"} onClick={() => setSelectedOption(-1)}>
+            Cook&Go
+          </Link>
         </h1>
-        <div className="pt-[4px] gap-x-3 flex flex-row">
+        <div className="mt-[3px] gap-x-3 flex flex-row">
           <Link
             href={"/admin/categoria"}
-            className="text-white font-light text-sm"
+            className={`text-white px-2 pt-[1px] font-light text-sm ${
+              selectedOption == 0 && "text-blue-600 bg-white rounded-md"
+            }`}
+            onClick={() => setSelectedOption(0)}
           >
             Mis categorias
           </Link>
-          <Link href={"/admin/menu"} className="text-white font-light text-sm">
+          <Link
+            href={"/admin/menu"}
+            className={`text-white px-2 pt-[1px] font-light text-sm ${
+              selectedOption == 1 && "text-blue-600 bg-white rounded-md"
+            }`}
+            onClick={() => setSelectedOption(1)}
+          >
             Mis menús
           </Link>
-          <Link href={"/admin/plato"} className="text-white font-light text-sm">
+          <Link
+            href={"/admin/plato"}
+            className={`text-white px-2 pt-[1px] font-light text-sm ${
+              selectedOption == 2 && "text-blue-600 bg-white rounded-md"
+            }`}
+            onClick={() => setSelectedOption(2)}
+          >
             Mis platos
           </Link>
           <Link
             href={"/admin/ingrediente"}
-            className="text-white font-light text-sm"
+            className={`text-white px-2 pt-[1px] font-light text-sm ${
+              selectedOption == 3 && "text-blue-600 bg-white rounded-md"
+            }`}
+            onClick={() => setSelectedOption(3)}
           >
             Mis ingredientes
           </Link>
