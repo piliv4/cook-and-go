00bbd286fa5f49690cc5b3e5738da612f9f8466f
piliv4 <79758821+piliv4@users.noreply.github.com>
2023-03-30 15:00:08 +0200

    refinando detalles de plato

--- src/pages/admin/plato/[id].tsx
+++ src/pages/admin/plato/[id].tsx
@@ -1,77 +1,129 @@
 import CabeceraPagina from "@/components/admins/ui/CabeceraPagina";
 import supabase from "@/server/client";
-import { Ingrediente, Plato } from "@/types/types";
+import Image from "next/image";
+import { Categoria, Ingrediente, Plato } from "@/types/types";
 import { GetServerSideProps } from "next";
+import { BsFillPencilFill, BsTrashFill } from "react-icons/bs";
+import CrearPlatoPopUp from "@/components/admins/plato/CrearPlatoPopUp";
+import { useState } from "react";
+import router from "next/router";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { id } = context.query;
-  let ingredientes: any[] = [];
   let { data: plato } = await supabase
     .from("Articulo")
     .select("*")
-    .eq("id", id);
+    .eq("id", id)
+    .single();
+  //RECUPERAMOS LOS INGREDIENTES
+  plato.ingredientes = [];
   let { data: ingredientesId } = await supabase
     .from("ArticuloIngrediente")
     .select("ingrediente_id")
-    .eq("articulo_id", id);
+    .eq("articulo_id", plato.id);
+
   if (ingredientesId != null) {
     for (const id of ingredientesId) {
       let { data: ingrediente } = await supabase
         .from("Ingrediente")
         .select("*")
         .eq("id", id.ingrediente_id);
-      ingrediente && ingredientes.push(ingrediente[0] as Ingrediente);
+      ingrediente && plato.ingredientes.push(ingrediente[0] as Ingrediente);
     }
   }
+  //RECUPERAMOS LA CATEGORIA
+  let { data: categoria } = await supabase
+    .from("Categoria")
+    .select("*")
+    .eq("id", plato.categoria)
+    .single();
+
   return {
     props: {
-      plato: plato && (plato[0] as Plato),
-      ingredientes: ingredientes as Ingrediente[],
+      plato: plato && (plato as Plato),
+      categoria: categoria as Categoria,
     },
   };
 };
 
 const DetallesPlato = ({
   plato,
-  ingredientes,
+  categoria,
 }: {
   plato: Plato;
-  ingredientes: any[];
+  categoria: Categoria;
 }) => {
+  const [open, setOpen] = useState(false);
+
+  async function borrarPlato() {
+    const { error } = await supabase
+      .from("Articulo")
+      .delete()
+      .eq("id", plato.id);
+    console.log(error);
+    if (!error) {
+      router.replace("/admin/platos");
+    }
+  }
   return (
     <div className="mx-48">
       <CabeceraPagina>
-        <h1 className="text-2xl font-black ">{plato.nombre}</h1>
+        <h1 className="text-2xl font-black uppercase col-span-2">
+          {plato.nombre}
+        </h1>
+        <div className="w-full flex flex-row gap-2 justify-end">
+          <BsFillPencilFill
+            className="group fill-primaryOrange hover:fill-secondaryOrange transition duration-150"
+            onClick={() => setOpen(true)}
+          />
+          <BsTrashFill
+            className="fill-primaryOrange hover:fill-secondaryOrange transition duration-150"
+            onClick={() => borrarPlato()}
+          />
+        </div>
       </CabeceraPagina>
       {/* Información general */}
-      <div className="grid grid-cols-2 pt-2">
-        <div>{/* IMAGEN */}</div>
-        <div>
-          <div className="grid grid-cols-2">
-            <p>Nombre:</p>
-            <h1>{plato.nombre}</h1>
+      <div className="grid grid-cols-2 pt-2 gap-x-8">
+        <div className="relative py-40">
+          <Image
+            src={plato.imagenURL}
+            alt="imagen_plato.jpg"
+            className="absolute rounded-md"
+            fill
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className="rounded-md border-[1px] border-secondaryGreen overflow-hidden">
+          <div className="w-full bg-secondaryGreen py-2 text-center text-lg font-black uppercase">
+            Información general del plato
           </div>
-          <div className="grid grid-cols-2">
-            <p>Descripción:</p>
-            <p>{plato.descripcion}</p>
-          </div>
-          <div className="grid grid-cols-2">
-            <p>Categoria:</p>
-            <p>{plato.categoria}</p>
-          </div>
-          <div className="grid grid-cols-2">
-            <p>Precio:</p>
-            <p>{plato.precio}</p>
+          <div className="p-4 gap-8 flex flex-col h-full">
+            <div className="flex flex-row border-b-[2px] border-primaryOrange border-dotted">
+              <p className="font-black">Nombre:</p>
+              <p className="w-full text-right">{plato.nombre}</p>
+            </div>
+            <div className="flex flex-row border-b-[2px] border-primaryOrange border-dotted">
+              <p className="font-black">Descripción:</p>
+              <p className="w-full text-right">{plato.descripcion}</p>
+            </div>
+            <div className="flex flex-row border-b-[2px] border-primaryOrange border-dotted">
+              <p className="font-black">Categoria:</p>
+              <p className="w-full text-right">{categoria?.nombre}</p>
+            </div>
+            <div className="flex flex-row border-b-[2px] border-primaryOrange border-dotted">
+              <p className="font-black">Precio:</p>
+              <p className="w-full text-right">{plato.precio}€</p>
+            </div>
           </div>
         </div>
       </div>
       {/* Mis ingredientes */}
-      <div className="flex flex-col gap-y-4">
-        <div className="pb-1 border-primaryGreen border-b-[2px] text-xl font-black">
+      <div className="flex flex-col gap-y-4 mt-4">
+        <div className="pb-1 border-primaryGreen border-b-[2px] text-xl font-black uppercase">
           Ingredientes:
         </div>
-        {ingredientes.length > 0 ? (
-          ingredientes.map((ingrediente, index) => (
+        {plato.ingredientes.length > 0 ? (
+          plato.ingredientes.map((ingrediente, index) => (
             <div
               key={ingrediente.id}
               className="flex flex-row border-b-[2px] border-primaryOrange border-dotted"
@@ -87,6 +139,11 @@ const DetallesPlato = ({
           </div>
         )}
       </div>
+      <CrearPlatoPopUp
+        platoEditar={plato}
+        cerrarPopUp={() => setOpen(false)}
+        open={open}
+      />
     </div>
   );
 };
