865aa15c1f0384341cfb1a3e215ec0e117d1c70a
piliv4 <79758821+piliv4@users.noreply.github.com>
2023-04-03 20:12:18 +0200

    avance en seleccionar platos para menú

--- src/components/admins/menu/SeccionMenu.tsx
+++ src/components/admins/menu/SeccionMenu.tsx
@@ -1,6 +1,20 @@
+import { Plato } from "@/types/types";
 import { useState } from "react";
+import { BiSearch } from "react-icons/bi";
 import { BsTrash } from "react-icons/bs";
-const SeccionMenu = ({ titulo }: { titulo: string }) => {
+const SeccionMenu = ({
+  titulo,
+  platos,
+  platosAnyadidos,
+  anyadirPlato,
+}: {
+  titulo: string;
+  platos: Plato[];
+  platosAnyadidos: Plato[];
+  anyadirPlato: Function;
+}) => {
+  console.log(platos);
+  const [value, setValue] = useState("");
   return (
     <div className="flex justify-center flex-col">
       <div className="font-black pt-4  border-b-[1px] border-primaryGreen flex flex-row">
@@ -9,11 +23,74 @@ const SeccionMenu = ({ titulo }: { titulo: string }) => {
         <input className="accent-primaryOrange " type="checkbox" />
       </div>
       <div className="">
-        <div className="flex flex-row py-1 border-b-[2px] border-primaryOrange border-dotted ">
-          <p className="w-full">Plato</p>
-          <BsTrash className=" self-end" />
+        {platosAnyadidos.map((plato) => (
+          <div
+            key={plato.id}
+            className="flex flex-row py-1 border-b-[2px] border-primaryOrange border-dotted "
+          >
+            <p className="w-full">{plato.nombre}</p>
+            <BsTrash className=" self-end" />
+          </div>
+        ))}
+      </div>
+
+      {/* SELECTOR DE PLATOS */}
+      <div className=" flex flex-col group  ">
+        <div className="bg-white grid grid-cols-[90%_10%] rounded-[30px] border-neutral  border-[1px] overflow-hidden">
+          <div className="px-2 py-1">
+            <input
+              className="input  w-full bg-transparent text-sm text-neutral focus:outline-0 sm:h-full"
+              autoComplete="off"
+              id="searchBar"
+              type="text"
+              value={value}
+              placeholder="Buscador de ingredientes..."
+              onChange={(e) => {
+                setValue(e.target.value);
+              }}
+            ></input>
+          </div>
+          <div className="flex items-center justify-center">
+            <BiSearch className="stroke-primaryOrange stroke-[2px]" />
+          </div>
         </div>
-        <p>Añadir plato</p>
+
+        <div className="hidden group-hover:flex hover:flex z-10 flex-col border-x-[1px] border-b-[1px]  mx-3 rounded-b-md bg-white ">
+          {Object.values(platos)
+            .filter((plato) => {
+              return (
+                plato &&
+                (plato.nombre?.toLowerCase().startsWith(value.toLowerCase()) ||
+                  plato.nombre
+                    ?.toLowerCase()
+                    .includes(" " + value.toLowerCase())) &&
+                plato.nombre.toLowerCase() !== value.toLowerCase()
+              );
+            })
+            .slice(0, 10)
+            .map((plato) => (
+              <div
+                className="grid grid-cols-[80%_20%] cursor-pointer pl-4  text-sm hover:bg-terciaryIntermediate"
+                key={plato.id}
+                onClick={() =>
+                  setValue(
+                    plato.nombre.charAt(0).toUpperCase() + plato.nombre.slice(1)
+                  )
+                }
+              >
+                <p>
+                  {plato.nombre.charAt(0).toUpperCase() + plato.nombre.slice(1)}
+                </p>
+                <p
+                  onClick={() => {
+                    anyadirPlato(plato);
+                  }}
+                >
+                  Añadir
+                </p>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
--- src/pages/admin/menu/crearMenu.tsx
+++ src/pages/admin/menu/crearMenu.tsx
@@ -1,8 +1,24 @@
 import SeccionMenu from "@/components/admins/menu/SeccionMenu";
 import CabeceraPagina from "@/components/admins/ui/CabeceraPagina";
+import supabase from "@/server/client";
+import { Plato } from "@/types/types";
+import { useState } from "react";
 
-export default function CrearMenu() {
+export async function getStaticProps() {
+  let { data: platos } = await supabase.from("Articulo").select("*");
+  return {
+    props: {
+      platos: platos as Plato[],
+    },
+  };
+}
+
+export default function CrearMenu(platos: Plato[]) {
   const menuCategorias = ["Entrantes", "Primeros", "Segundos", "Postres"];
+  const [entrantes, setEntrantes] = useState([]);
+  const [primeros, setPrimeros] = useState([]);
+  const [segundos, setSegundos] = useState([]);
+  const [postres, setPostres] = useState([]);
   return (
     <div className="px-48 ">
       <CabeceraPagina>
@@ -12,16 +28,47 @@ export default function CrearMenu() {
       </CabeceraPagina>
       <div>
         <h1>Información general</h1>
+        <div className="flex flex-col gap-y-[1px] w-full">
+          <p className="font-thin">Nombre</p>
+          <input type={"text"} className="px-6 border-[1px] rounded-md" />
+        </div>
+        <div className="flex flex-col gap-y-[1px] w-full">
+          <p className="font-thin">Precio</p>
+          <input type={"tel"} className="px-6 border-[1px] rounded-md" />
+        </div>
       </div>
       {/* SECCIONES DEL MENU */}
       <div className=" flex flex-col">
-        {menuCategorias.map((categoria) => (
-          <SeccionMenu titulo={categoria} key={categoria} />
-        ))}
+        <SeccionMenu
+          titulo={"Entrantes"}
+          platos={platos}
+          anyadirPlato={setEntrantes}
+          platosAnyadidos={entrantes}
+        />
       </div>
-      <div>
-        <button>Guardar</button>
-        <button>Cancelar</button>
+      <div className="flex flex-col gap-y-2">
+        <div className="flex flex-row border-b-[1px] border-primaryGreen">
+          <p className="w-full font-black">¿Incluir pan?</p>
+          <label className="flex flex-row gap-x-1 font-light">
+            Incluir
+            <input type="checkbox" />
+          </label>
+        </div>
+        <div className="flex flex-row border-b-[1px] border-primaryGreen">
+          <p className="w-full font-black">¿Incluir bebida?</p>
+          <label className="flex flex-row gap-x-1 font-light">
+            Incluir
+            <input type="checkbox" />
+          </label>
+        </div>
+      </div>
+      <div className=" flex flex-row justify-end gap-x-2 font-black">
+        <button className=" ml-3 mt-3 rounded-full border text-white border-primaryOrange bg-primaryOrange px-1 hover:scale-105 transition duration-100 sm:mt-5 sm:px-3">
+          Guardar
+        </button>
+        <button className=" ml-3 mt-3 rounded-full border border-primaryOrange bg-transparent px-1 hover:scale-105 transition duration-100 sm:mt-5 sm:px-3">
+          Cancelar
+        </button>
       </div>
     </div>
   );
