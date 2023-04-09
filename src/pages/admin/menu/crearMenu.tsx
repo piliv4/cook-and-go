faff45544ad4fa6b75f821c6172d0be9b1faba3e
piliv4 <79758821+piliv4@users.noreply.github.com>
2023-04-09 12:58:23 +0200

    Corregido la opción de crear menu

--- src/pages/admin/menu/crearMenu.tsx
+++ src/pages/admin/menu/crearMenu.tsx
@@ -1,3 +1,4 @@
+import MenuFormulario from "@/components/admins/menu/MenuFormulario";
 import SeleccionarPlatos from "@/components/admins/menu/SeleccionarPlatos";
 import CabeceraPagina from "@/components/admins/ui/CabeceraPagina";
 import supabase from "@/server/client";
@@ -15,36 +16,23 @@ export async function getStaticProps() {
 }
 
 export default function CrearMenu({ platos }: { platos: Plato[] }) {
-  const [menu, setMenu] = useState<Menu>({
-    id: "",
-    nombre: "",
-    precio: 0,
-    comensales: 0,
-    incluyePan: false,
-    incluyeBebida: false,
-    entrantes: [],
-    primeros: [],
-    segundos: [],
-    postres: [],
-  });
-  const [errorTitulo, setErrorTitulo] = useState("");
-  const [errorPrecio, setErrorPrecio] = useState("");
-  const [errorComensales, setErrorComensales] = useState("");
   const tiposPlato = ["entrantes", "primeros", "segundos", "postres"];
-  async function crearMenu() {
+  async function crearMenu(menu: Menu) {
+    console.log(menu);
     const { data, error } = await supabase
       .from("Menu")
       .insert([
         {
           nombre: menu.nombre,
           precio: menu.precio,
-          num_personas: menu.comensales,
+          comensales: menu.comensales,
           incluye_pan: menu.incluyePan,
           incluye_bebida: menu.incluyeBebida,
           restaurante_id: "ea443834-c2ff-45e9-9504-ab580bcbbe01",
         },
       ])
       .select();
+    console.log(error);
     if (!error) {
       tiposPlato.map((tipoPlato) =>
         insertarPlatos(
@@ -54,39 +42,6 @@ export default function CrearMenu({ platos }: { platos: Plato[] }) {
         )
       );
       router.push("/admin/menu");
-    }
-  }
-
-  function validacionCampos() {
-    setErrorComensales("");
-    setErrorPrecio("");
-    setErrorTitulo("");
-    if (menu.nombre == "" || menu.nombre == null) {
-      setErrorTitulo("Introduzca un titulo");
-    }
-    if (menu.precio == null || menu.precio <= 0) {
-      setErrorPrecio("Introduzca un precio");
-    }
-    if (menu.comensales == null || menu.comensales <= 0) {
-      setErrorComensales("Introduzca un número de comensales");
-    }
-    if (
-      menu.entrantes.length <= 0 &&
-      menu.primeros.length <= 0 &&
-      menu.segundos.length <= 0 &&
-      menu.postres.length <= 0
-    ) {
-      return false;
-    }
-    if (errorComensales != "" || errorPrecio != "" || errorTitulo != "") {
-      return false;
-    }
-    return true;
-  }
-
-  function guardar() {
-    if (validacionCampos()) {
-      crearMenu();
     }
   }
 
@@ -111,137 +66,7 @@ export default function CrearMenu({ platos }: { platos: Plato[] }) {
     }
   }
 
-  function anyadirPlato(plato: Plato, tipoPlato: string) {
-    let key = tipoPlato as keyof Menu;
-
-    !(menu[key] as Plato[]).includes(plato) &&
-      setMenu({ ...menu, [key]: (menu[key] as Plato[]).concat([plato]) });
-  }
-
-  function eliminarPlatoPorIndice(index: number, tipoPlato: string) {
-    let key = tipoPlato as keyof Menu;
-    setMenu({
-      ...menu,
-      [key]: (menu[key] as Plato[]).filter((value, i) => i !== index),
-    });
-  }
-
-  function eliminarPlatos(tipoPlato: string) {
-    let key = tipoPlato as keyof Menu;
-    setMenu({
-      ...menu,
-      [key]: [],
-    });
-  }
-
   return (
-    <div className="px-48 ">
-      <CabeceraPagina>
-        <h1 className="text-2xl font-black col-span-3 text-center uppercase ">
-          Crear un nuevo menú
-        </h1>
-      </CabeceraPagina>
-      <div>
-        <h1 className="w-full border-b-2 pb-1 text-center pt-4 font-black text-lg border-primaryGreen">
-          INFORMACIÓN GENERAL
-        </h1>
-        <div>
-          <div className="flex flex-col gap-y-[1px] w-full pt-2">
-            <p className="">Nombre</p>
-            <input
-              type={"text"}
-              className="px-6  border-[1px] rounded-md"
-              onChange={(e) => setMenu({ ...menu, nombre: e.target.value })}
-            />
-            <p className="text-red-600 font-medium">{errorTitulo}</p>
-          </div>
-          <div className="flex pt-2 flex-row gap-x-4">
-            <div className="flex flex-col gap-y-[1px] w-full">
-              <p className="">Precio</p>
-              <input
-                type={"tel"}
-                className="px-6 border-[1px] rounded-md"
-                onChange={(e) =>
-                  setMenu({ ...menu, precio: parseFloat(e.target.value) })
-                }
-              />
-              <p className="text-red-600 font-medium">{errorPrecio}</p>
-            </div>
-            <div className="flex flex-col gap-y-[1px] w-full">
-              <p className="">Número de comensales</p>
-              <input
-                type="number"
-                className="px-6 border-[1px] rounded-md"
-                onChange={(e) =>
-                  setMenu({ ...menu, comensales: parseInt(e.target.value) })
-                }
-              />
-              <p className="text-red-600 font-medium">{errorComensales}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* SECCIONES DEL MENU */}
-
-      <div className=" flex flex-col">
-        <h1 className="w-full border-b-2 pb-1 pt-6 text-center font-black text-lg border-primaryGreen">
-          PLATOS
-        </h1>
-        {tiposPlato.map((tipoPlato) => (
-          <SeleccionarPlatos
-            key={tipoPlato}
-            titulo={tipoPlato}
-            platos={platos}
-            eliminarPlatoPorIndice={eliminarPlatoPorIndice}
-            eliminarPlatos={eliminarPlatos}
-            anyadirPlato={anyadirPlato}
-            platosAnyadidos={menu[tipoPlato as keyof Menu] as Plato[]}
-          />
-        ))}
-      </div>
-      <h1 className="w-full border-b-2 pb-1 pt-6 text-center font-black text-lg border-primaryGreen">
-        EXTRAS
-      </h1>
-      <div className="flex flex-col gap-y-4 pt-4">
-        <div className="flex flex-row border-b-[1px] border-primaryGreen">
-          <p className="w-full font-black">¿Incluir pan?</p>
-          <label className="flex flex-row gap-x-1 font-light">
-            Incluir
-            <input
-              type="checkbox"
-              onChange={(e) =>
-                setMenu({ ...menu, incluyePan: e.target.checked })
-              }
-            />
-          </label>
-        </div>
-        <div className="flex flex-row border-b-[1px] border-primaryGreen">
-          <p className="w-full font-black">¿Incluir bebida?</p>
-          <label className="flex flex-row gap-x-1 font-light">
-            Incluir
-            <input
-              type="checkbox"
-              onChange={(e) =>
-                setMenu({ ...menu, incluyeBebida: e.target.checked })
-              }
-            />
-          </label>
-        </div>
-      </div>
-      <div className=" flex flex-row justify-end gap-x-2 font-black py-4">
-        <button
-          className=" ml-3 mt-3 rounded-full border text-white border-primaryOrange bg-primaryOrange px-1 hover:scale-105 transition duration-100 sm:mt-5 sm:px-3"
-          onClick={() => guardar()}
-        >
-          Guardar
-        </button>
-        <button
-          className=" ml-3 mt-3 rounded-full border border-primaryOrange bg-transparent px-1 hover:scale-105 transition duration-100 sm:mt-5 sm:px-3"
-          onClick={() => router.push("/admin/menu")}
-        >
-          Cancelar
-        </button>
-      </div>
-    </div>
+    <MenuFormulario crearEditar={crearMenu} menuProp={null} platos={platos} />
   );
 }
