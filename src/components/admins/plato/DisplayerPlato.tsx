54f6c9406e50480467cf646cf53c37265108e338
piliv4 <79758821+piliv4@users.noreply.github.com>
2023-06-21 17:06:22 +0200

    lol?

--- src/components/admins/plato/DisplayerPlato.tsx
+++ src/components/admins/plato/DisplayerPlato.tsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
 import CrearPlatoCard from "./CrearPlatoCard";
 import PlatoCard from "./PlatoCard";
-import { useRouter } from "next/router";
 import { Plato } from "@/types/Plato";
 
 const DisplayerPlato = ({ platos }: { platos: Plato[] }) => {
-  const router = useRouter();
-
   return (
     <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-1 sm:gap-3 ">
       <CrearPlatoCard />
