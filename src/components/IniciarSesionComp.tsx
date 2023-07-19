470d2bb74c1fc5db9fe1e49065405273a59f90c4
piliv4 <79758821+piliv4@users.noreply.github.com>
2023-07-19 19:31:30 +0200

    full responsive

--- src/components/IniciarSesionComp.tsx
+++ src/components/IniciarSesionComp.tsx
@@ -51,13 +51,15 @@ const IniciarSesionComp = () => {
 
   return (
     <div className="h-screen w-full flex justify-center items-center bg-terciaryIntermediate">
-      <div className="rounded-lg bg-background shadow-xl grid grid-cols-[45%_55%]">
-        <div className="m-3 bg-primaryGreen rounded-lg flex justify-center items-center flex-col px-4 py-10">
+      <div className="rounded-lg bg-background shadow-xl grid grid-cols-[45%_55%] sm:mx-0 mx-3">
+        <div className="m-3 bg-primaryGreen rounded-lg flex justify-center items-center flex-col sm:px-4 sm:py-10">
           <FaUtensils size={46} className="fill-white" />
 
-          <p className="font-black text-5xl px-6 text-white">Cook&Go</p>
+          <p className="font-black text-2xl sm:text-5xl sm:px-6 text-white">
+            Cook&Go
+          </p>
         </div>
-        <div className="my-10 mr-3 px-3 flex flex-col">
+        <div className="my-10 mr-3 sm:px-3 flex flex-col">
           <h1 className="text-center text-2xl font-black">¡Bienvenido!</h1>
           <h2 className="text-sm font-medium text-center mt-1 text-gray-900">
             Inicie sesión para empezar a gestionar
