eb5d8b1d07b8f524e7cd8c140fac41be76c7c369
piliv4 <79758821+piliv4@users.noreply.github.com>
2023-09-11 18:54:15 +0200

    mb-4

--- src/components/admins/plato/DisplayerPlato.tsx
+++ src/components/admins/plato/DisplayerPlato.tsx
@@ -4,7 +4,7 @@ import { Plato } from "@/types/Plato";
 
 const DisplayerPlato = ({ platos }: { platos: Plato[] }) => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-1 sm:gap-3 ">
+    <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-1 sm:gap-3 mb-4">
       <CrearPlatoCard />
       {platos.map((plato) => (
         <PlatoCard plato={plato} key={plato.id} />
